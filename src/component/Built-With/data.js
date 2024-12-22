38a3a79b100af011f98e30d2f6c84da59218aae1
codingshot <45281667+codingshot@users.noreply.github.com>
2024-12-22 08:14:15 -0800

    commented out built with
    - should detele this in total
    
    need topoptizie wiht less finromation and load times

--- src/component/Built-With/data.js
+++ src/component/Built-With/data.js
@@ -7,18 +7,18 @@ const technologies = [
     img: "/img/built-with/near.png",
     url: "https://near.org/",
   },
-  {
-    img: "/img/built-with/polygon.png",
-    url: "https://polygon.technology/",
-  },
-  {
-    img: "/img/built-with/react.png",
-    url: "https://reactjs.org/",
-  },
-  {
-    img: "/img/built-with/holaplex.png",
-    url: "https://www.holaplex.com/",
-  },
+  // {
+  //   img: "/img/built-with/polygon.png",
+  //   url: "https://polygon.technology/",
+  // },
+  // {
+  //   img: "/img/built-with/react.png",
+  //   url: "https://reactjs.org/",
+  // },
+  // {
+  //   img: "/img/built-with/holaplex.png",
+  //   url: "https://www.holaplex.com/",
+  // },
   {
     img: "/img/built-with/astro.png",
     url: "https://astrodao.com/",
@@ -35,71 +35,71 @@ const technologies = [
     img: "/img/built-with/poap.png",
     url: "https://poap.xyz",
   },
-  {
-    img: "/img/built-with/nearsocial.png",
-    url: "https://near.social",
-  },
-  {
-    img: "/img/built-with/keypom.png",
-    url: "https://keypom.xyz/",
-  },
-  {
-    img: "/img/built-with/genadrops.png",
-    url: "https://genadrop.io/mint/sesh",
-  },
-  {
-    img: "/img/built-with/underdog.png",
-    url: "https://underdogprotocol.com/",
-  },
-  {
-    img: "/img/built-with/paras.png",
-    url: "https://paras.id/",
-  },
-  {
-    img: "/img/built-with/IPFS.png",
-    url: "https://ipfs.io/",
-  },
-  {
-    img: "/img/built-with/areweave.png",
-    url: "https://www.arweave.org/",
-  },
-  {
-    img: "/img/built-with/cupcake.png",
-    url: "https://cupcake.com/",
-  },
-    {
-    img: "/img/built-with/soap-wordmark-black.png",
-    url: "https://solanasoap.lol/",
-  },
-  {
-    img: "/img/built-with/open-sea.png",
-    url: "https://opensea.io/",
-  },
-  {
-    img: "/img/built-with/matrica.png",
-    url: "https://matrica.io/",
-  },
+  // {
+  //   img: "/img/built-with/nearsocial.png",
+  //   url: "https://near.social",
+  // },
+  // {
+  //   img: "/img/built-with/keypom.png",
+  //   url: "https://keypom.xyz/",
+  // },
+  // {
+  //   img: "/img/built-with/genadrops.png",
+  //   url: "https://genadrop.io/mint/sesh",
+  // },
+  // {
+  //   img: "/img/built-with/underdog.png",
+  //   url: "https://underdogprotocol.com/",
+  // },
+  // {
+  //   img: "/img/built-with/paras.png",
+  //   url: "https://paras.id/",
+  // },
+  // {
+  //   img: "/img/built-with/IPFS.png",
+  //   url: "https://ipfs.io/",
+  // },
+  // {
+  //   img: "/img/built-with/areweave.png",
+  //   url: "https://www.arweave.org/",
+  // },
+  // {
+  //   img: "/img/built-with/cupcake.png",
+  //   url: "https://cupcake.com/",
+  // },
+  //   {
+  //   img: "/img/built-with/soap-wordmark-black.png",
+  //   url: "https://solanasoap.lol/",
+  // },
+  // {
+  //   img: "/img/built-with/open-sea.png",
+  //   url: "https://opensea.io/",
+  // },
+  // {
+  //   img: "/img/built-with/matrica.png",
+  //   url: "https://matrica.io/",
+  // },
 
-  {
-    img: "/img/built-with/satori.png",
-    url: "https://satori.art/",
-  },
-  {
-    img: "/img/built-with/metaplex.png",
-    url: "https://www.metaplex.com/",
-  },
-  {
-    img: "/img/built-with/nepbot.png",
-    url: "https://nepbot.org/",
-  },
-  {
-    img: "/img/built-with/wonderverse.png",
-    url: "https://app.wonderverse.xyz/organization/bluntda/boards",
-  },
-  {
-    img: "/img/built-with/web3auth.png",
-    url: "https://web3auth.io/",
-  },
+  // {
+  //   img: "/img/built-with/satori.png",
+  //   url: "https://satori.art/",
+  // },
+  // {
+  //   img: "/img/built-with/metaplex.png",
+  //   url: "https://www.metaplex.com/",
+  // },
+  // {
+  //   img: "/img/built-with/nepbot.png",
+  //   url: "https://nepbot.org/",
+  // },
+  // {
+  //   img: "/img/built-with/wonderverse.png",
+  //   url: "https://app.wonderverse.xyz/organization/bluntda/boards",
+  // },
+  // {
+  //   img: "/img/built-with/web3auth.png",
+  //   url: "https://web3auth.io/",
+  // },
 ];
 
 export default technologies;
